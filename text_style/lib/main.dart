2cd8ee593b4fbba5d46105d35ebc5cb1e517f782
GizeleAlves <gizele0alves@gmail.com>
2024-05-21 07:44:30 -0300

    Estilizando textos e importando fontes.

---
+++ text_style/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Nazalization',),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Aplicativo Exemplo Scaffold',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Text(
+            'Texto 1\nTexto 2',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              fontSize: 50,
+              /*color: Colors.black,
+              height: 2,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.yellow.shade700,
+              decoration: TextDecoration.underline,*/
+            ),
+          ),
+        ),
+      ),
+    );
+
+    /*Container(
+      child: Center(
+        child: Text(
+          'Texto 1\nTexto 2',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 50,
+            height: 2,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.yellow.shade700,
+            decoration: TextDecoration.underline,
+            fontFamily: 'Nazalization',
+          ),
+        ),
+      ),
+    );*/
+  }
+}
