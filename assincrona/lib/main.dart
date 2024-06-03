fac04e6340bbfaee43cf03c9715703c34f50e81a
GizeleAlves <gizele0alves@gmail.com>
2024-06-03 10:48:04 -0300

    Await e async

--- assincrona/lib/main.dart
+++ assincrona/lib/main.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
 
-void main() {
-
+Future<void> main() async {
   print('1');
   print(2);
   print(3);
- 
-  Future<void> quatro = Future.delayed(
+  //print(4);
+
+  /*Future<void> quatro = Future.delayed(
     Duration(seconds: 3),
   );
-  quatro.then((value) => print(4));
-  //print(4);
+  quatro.then((value) => print(4));*/
+  await quatro();
   print(5);
+}
+
+Future<void> quatro() async {
+  Future.delayed(Duration(seconds: 3), ()=> print(4));
 }
