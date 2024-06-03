f2909160114642edfc365f8d840e209ef2e1d446
GizeleAlves <gizele0alves@gmail.com>
2024-06-03 14:47:34 -0300

    Lendo múltiplos dados em json.

--- requisicao_http_json/lib/main.dart
+++ requisicao_http_json/lib/main.dart
@@ -3,15 +3,21 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() async {
-  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/');
   final future = http.get(uri);
   future.then((response) {
     if (response.statusCode == 200) {
-      print('Página carregada.');
+      print('Página carregada ok.');
+
       // print(json.decode(response.body));
-      Map<String, dynamic> dados = json.decode(response.body);
+      /*Map<String, dynamic> dados = json.decode(response.body);
       print(dados);
-      print(dados['title']);
+      print(dados['title']);*/
+
+      var list = json.decode(response.body) as List;
+      list.forEach((element) {
+        print(element['title']);
+      });
     } else {
       print('Deu erro!');
     }
